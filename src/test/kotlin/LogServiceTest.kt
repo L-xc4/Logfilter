8f05e7721893be6b43652f97dcbf39ca81fe1507
L-xc4 <luca-pascale@outlook.com>
2026-04-18 13:54:39 +0200

    Updatet

---
+++ src/test/kotlin/LogServiceTest.kt
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+
+
+class LogServiceTest {
+    @Test
+    fun `filterLogs should return only errors when condition matches`() {
+        // 1. Arrange (Daten vorbereiten)
+        val data = listOf(
+            LogEntry("INFO", "Alles ok", 1L),
+            LogEntry("ERROR", "Fehler!", 2L)
+        )
+
+        // 2. Act (Die Funktion aufrufen)
+        val result = LogService.filterLogs(data) { it.level == "ERROR" }
+
+        // 3. Assert (Prüfen: Erwarten wir 1 Ergebnis?)
+        assertEquals(36, result.length)
+    }
+
+    @Test
+    fun `filterLogs should return empty string with no  matching data`() {
+        // 1. Arrange (Daten vorbereiten)
+        val data = listOf(
+            LogEntry("INFO", "Alles ok", 1L),
+            LogEntry("INFO", "Fehler!", 2L)
+        )
+
+        // 2. Act (Die Funktion aufrufen)
+        val result = LogService.filterLogs(data) { it.level == "ERROR" }
+
+        // 3. Assert (Prüfen: Erwarten wir 1 Ergebnis?)
+        assertEquals(0, result.length)
+    }
+
+}
